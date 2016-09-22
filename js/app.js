46eb965fc1a740f35fec6b972ede601ba1fdbc5c
AmySeo12 <maryori.amy@gmail.com>
2016-09-22 13:47:44 -0500

    subiendo la version 3

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", function(){
+	var padre = document.getElementById("padre")
 	var lista = document.getElementById("lista");
     var formulario = document.getElementById("formulario");
     var button = document.getElementById("boton");
@@ -9,11 +10,19 @@ window.addEventListener("load", function(){
 
     button.addEventListener("click", function(e){
     	e.preventDefault();
-    	var texto = "Añadir una tarjeta...";
-    	lista.classList.add("none");
-    	crear("div", lista.value,formulario, "negrita");
-    	crear("div", texto, formulario, "grey");
-    	agregar(button, close);
+    	var texto = "<a>Añadir una tarjeta...</>";
+    	formulario.classList.toggle("none");
+
+    	var contenedor = document.createElement("div");
+    	contenedor.classList.add("formulario");
+    	contenedor.classList.add("width");
+    	contenedor.classList.add("inline-block");
+    	padre.appendChild(contenedor);
+
+    	crear("div", lista.value, contenedor, "negrita");
+    	crear("div", texto, contenedor, "grey");
+    	agregarNuevaLista(padre);
+    	contador ++;
     });
 
     var close = document.getElementById("close");
@@ -22,9 +31,16 @@ window.addEventListener("load", function(){
     	agregar(formulario, div);
     });
 
+    function agregarNuevaLista(padre){
+    	var div= document.createElement("div");
+    	div.innerHTML="Añadir una lista..."
+    	div.classList.add("lista");
+    	padre.appendChild(div);
+    }
+
     function crear(div, contenido,padre, clase1){
     	var titulo = document.createElement(div);
-    	titulo.textContent = contenido;
+    	titulo.innerHTML = contenido;
     	titulo.classList.add("padding");
     	titulo.classList.add(clase1);
     	padre.appendChild(titulo)
@@ -34,4 +50,5 @@ window.addEventListener("load", function(){
     	formulario.classList.toggle("none");
     	div.classList.toggle("none");
     }
-})
+
+});
