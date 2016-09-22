3f85383c8e301a04f27d795d8bd2142cc6caea9b
AmySeo12 <maryori.amy@gmail.com>
2016-09-22 10:30:33 -0500

    subiendo la segunda version

--- js/app.js
+++ js/app.js
@@ -7,23 +7,28 @@ window.addEventListener("load", function(){
     	agregar(formulario, div);
     });
 
-    /*button.addEventListener("click", function(e){
+    button.addEventListener("click", function(e){
     	e.preventDefault();
+    	var texto = "Añadir una tarjeta...";
     	lista.classList.add("none");
-    	var titulo = document.createElement("div");
-    	titulo.textContent = lista.value;
-    	var tarjeta = document.createElement("div");
-    	tarjeta.textContent="Añadir una tarjeta";
-    	formulario.appendChild(titulo);
-    	formulario.appendChild(tarjeta);
+    	crear("div", lista.value,formulario, "negrita");
+    	crear("div", texto, formulario, "grey");
     	agregar(button, close);
-    });*/
+    });
 
     var close = document.getElementById("close");
     close.addEventListener("click", function(e) {
     	e.preventDefault();
     	agregar(formulario, div);
     });
+
+    function crear(div, contenido,padre, clase1){
+    	var titulo = document.createElement(div);
+    	titulo.textContent = contenido;
+    	titulo.classList.add("padding");
+    	titulo.classList.add(clase1);
+    	padre.appendChild(titulo)
+    }
 
     function agregar(formulario, div){
     	formulario.classList.toggle("none");
